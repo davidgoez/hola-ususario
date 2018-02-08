e198fe3e7ef26ee09ab13aa9104e8f450223ae2b
davidgoez <davidgoez.sistemas@gmail.com>
2018-02-08 07:17:50 -0500

    first commit

---
+++ src/hola/usuariio/HolaUsuariio.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.usuariio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class HolaUsuariio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+System.out.println("Introducir nombre del usuario ");
+
+String x ;
+Scanner b = new Scanner(System.in);
+        x = b.next();
+
+System.out.println("hola "+ x);
+
+// TODO code application logic here
+    }
+    
+}
